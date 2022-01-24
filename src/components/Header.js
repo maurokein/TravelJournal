017e6cf2a3343c3d07446bea8b073d704bc50208
maurokein <maurokein1990@gmail.com>
2022-01-24 13:55:18 -0300

    header component created and styled

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header() {
+    return(
+        <div className='container'>
+            <nav className='container--title'>
+                <i class="fa fa-globe fa-3x" aria-hidden="true"></i>
+                <h3 className='container--title__text'>My Travel Journey</h3>
+            </nav>
+        </div>
+    )
+}
